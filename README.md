b18ede5c5d1f737010faf5de5c5654556a12153a
Brian Kiprono <89153335+bkiprono@users.noreply.github.com>
2023-09-18 17:09:36 +0300

    feat: added logic to request auth

---
+++ README.md
@@ -0,0 +1,3 @@
+# nodejs-google-drive
+
+This is a sample node js integration for google drive api
---
+++ index.js
@@ -0,0 +1,83 @@
+const { google } = require('googleapis');
+const fs = require('fs');
+
+// Load client secrets from a file you obtained from the Google API Console
+const credentials = require('./client_secret.json');
+
+// Create an OAuth2 client
+const { client_secret, client_id, redirect_uris } = credentials.installed;
+const oAuth2Client = new google.auth.OAuth2(client_id, client_secret, redirect_uris[0]);
+
+// Define the scope for Google Drive
+const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
+
+// Check if we have previously stored a token
+fs.readFile('token.json', (err, token) => {
+  if (err) {
+    getAccessToken(oAuth2Client);
+  } else {
+    oAuth2Client.setCredentials(JSON.parse(token));
+    uploadFile(oAuth2Client);
+  }
+});
+
+// Function to get an access token
+function getAccessToken(oAuth2Client) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+
+  console.log('Authorize this app by visiting this URL:', authUrl);
+
+  const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  readline.question('Enter the code from that page here: ', (code) => {
+    readline.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error retrieving access token', err);
+
+      oAuth2Client.setCredentials(token);
+
+      // Store the token to disk for later program executions
+      fs.writeFile('token.json', JSON.stringify(token), (err) => {
+        if (err) console.error('Error writing token to file', err);
+        console.log('Token stored to token.json');
+      });
+
+      uploadFile(oAuth2Client);
+    });
+  });
+}
+
+// Function to upload a file to Google Drive
+function uploadFile(auth) {
+  const drive = google.drive({ version: 'v3', auth });
+
+  const fileMetadata = {
+    name: 'example.txt', // Change this to the name of your file
+  };
+
+  const media = {
+    mimeType: 'text/plain', // Change this to the appropriate MIME type
+    body: fs.createReadStream('example.txt'), // Change this to the path of your file
+  };
+
+  drive.files.create(
+    {
+      resource: fileMetadata,
+      media: media,
+      fields: 'id',
+    },
+    (err, file) => {
+      if (err) {
+        console.error('Error uploading file:', err);
+      } else {
+        console.log('File ID:', file.data.id);
+      }
+    }
+  );
+}
